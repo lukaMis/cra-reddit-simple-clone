4706b5a2a67c90682ee4241dc3b732ae2dde65a1
Luka Mis <luka.mis@gmail.com>
2017-09-03 18:59:35 +0200

    Added new AddPost component and route to add posts.

---
+++ src/components/AddPost/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+class AddPost extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(e) {
+    const val = e.target.value
+    this.setState({
+      title: val
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    this.props.firebaseRef.push({
+      title: this.state.title
+    })
+    this.setState({
+      title: ''
+    })
+  }
+
+  render () {
+    return (
+      <div className='AddPost'>
+        <input type='text' placeholder='Write title of your post' value={this.state.title} onChange={this.handleChange} />
+        <div>
+          <button onClick={this.handleSubmit}>Submit</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default AddPost
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { Router, Route, browserHistory } from 'react-router'
 
 import App from './containers/App'
 import Posts from './components/Posts'
+import AddPost from './components/AddPost'
 
 const Routes = (props) => (
   <Router {...props} history={browserHistory}>
     <Route path='/' component={App}>
       <Route path='/posts' component={Posts} />
+      <Route path='/add-post' component={AddPost} />
     </Route>
   </Router>
 )
